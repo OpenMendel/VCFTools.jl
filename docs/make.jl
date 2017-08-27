9d8d2eb56422b7b2ec73517127f017feed1e15ce
Hua Zhou <huazhou@ucla.edu>
2017-08-26 22:08:19 -0700

    command for building docs locally

--- docs/make.jl
+++ docs/make.jl
@@ -8,3 +8,26 @@ deploydocs(
   julia  = "0.6",
   osname = "osx"
   )
+
+# ## for local build
+# makedocs(
+# #      doctest   = false,
+#       format    = :html,
+#       clean     = true,
+#       sitename  = "VCFTools.jl",
+#       modules   = [VCFTools],
+#       pages     = [
+#           "Home"  => "index.md",
+# 	  "VCF summary" => "summaryinfo.md",
+#      "Match markers in two VCF" => "conformgt.md",
+#      "API" => "api.md"
+#       ]
+# )
+#
+# deploydocs(
+#       repo    = "github.com:OpenMendel/VCFTools.jl.git",
+#       target  = "build",
+#       julia   = "0.6",
+#       deps    = nothing,
+#       make    = nothing
+# )
