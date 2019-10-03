00d9daa2de40d3e07227cedb2c03452ad197c792
Unknown <benchu99@hotmail.com>
2019-10-02 18:41:26 -0700

    changed REQUIRE to Project.toml file and remove NullableArrays

--- src/VCFTools.jl
+++ src/VCFTools.jl
@@ -1,16 +1,19 @@
 module VCFTools
 
-using CodecZlib, Distributions, GeneticVariation.VCF, HypothesisTests,
-    NullableArrays, ProgressMeter
+	using CodecZlib
+	using Distributions
+	using HypothesisTests
+	using ProgressMeter
+	import GeneticVariation.VCF
 
-export conformgt_by_id, conformgt_by_pos,
-    convert_gt, copy_gt!,
-    filter_genotype, gtstats,
-    nrecords, nsamples, openvcf
+	export conformgt_by_id, conformgt_by_pos,
+	    convert_gt, copy_gt!,
+	    filter_genotype, gtstats,
+	    nrecords, nsamples, openvcf
 
-# package code goes here
-include("gtstats.jl")
-include("conformgt.jl")
-include("convert.jl")
+	# package code goes here
+	include("gtstats.jl")
+	include("conformgt.jl")
+	include("convert.jl")
 
 end # module
